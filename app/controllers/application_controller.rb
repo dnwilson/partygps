e3d7d8b13864f69d57a8502e543730631bceae78
Dane Wilson <dwilson@td1000366.local>
2014-09-29 12:10:02 -0400

    Added methods for token authenticate via devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,27 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  # This is our new function that comes before Devise's one
+  before_filter :authenticate_user_from_token!
+  # This is Devise's authentication
+  before_filter :authenticate_user!
+
   # For CanCan authorization failure 
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to main_app.root_path, :alert => exception.message
+  end
+ 
+  private
+  
+  def authenticate_user_from_token!
+    user_email = params[:user_email].presence
+    user       = user_email && User.find_by_email(user_email)
+ 
+    # Notice how we use Devise.secure_compare to compare the token
+    # in the database with the token given in the params, mitigating
+    # timing attacks.
+    if user && Devise.secure_compare(user.authentication_token, params[:user_token])
+      sign_in user, store: false
+    end
   end
 end
